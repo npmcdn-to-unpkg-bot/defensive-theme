c5c04f8c4fab7e2df20072d09f1c65cf15adc3ff
Alex Goodkind <alex@rowrz.com>
2014-11-20 09:01:34 -0500

    moved defensive for easier tumblr handling

---
+++ defensive-js-assets.js
@@ -0,0 +1,19 @@
+var $themeButton = '<a target="_blank" id="install-button" class="btn icon" href="https://www.tumblr.com/theme/38849"><span id="install-theme-text">Install Theme</span></a>';
+$(document).ready(function() {
+  $(function() {
+    $("span#inner").replaceWith('<a target="_blank" class="nav" href="http://rowrz.tumblr.com">theme</a>')
+    $("body").prepend($themeButton);
+    $('#install-button').animate({top: 2.5});
+  })
+});
+var columnsObj = {
+  one: function() {
+    $("#posts").css("width", "400px");
+  },
+  two: function() {
+    $("#posts").css("width", "620px");
+  },
+  three: function() {
+    $("#posts").css("max-width", "80%");
+  }
+}
