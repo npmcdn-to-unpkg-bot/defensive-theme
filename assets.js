8dcc4f04f1985be938e89d3f6ffbda95f068dba0
Alex Goodkind <rowrz@icloud.com>
2014-12-26 17:25:01 -0500

    clean up, script push

--- assets.js
+++ assets.js
@@ -16,9 +16,8 @@ $(window).load(function() {
 });
 $(document).ready(function() {
   $(function() {
-    $("nav").append('<a target="_blank" class="nav" href="http://rowrz.tumblr.com/themes">theme</a>');
-    $("body").prepend('<a id="install-btn" href="https://www.tumblr.com/theme/38849" title="Install This Theme" target="_blank" style="opacity: 1;"><span id="install-btn-txt">Install Theme</span></a>');
+    $("nav").append('<a target="_blank" class="nav" href="http://rowrz.cf/1ss0vPg">theme</a>');
+    $("body").prepend('<a id="install-btn" href="http://rowrz.cf/1zYUFdI" title="Install This Theme" target="_blank" style="opacity: 1;"><span id="install-btn-txt">Install Theme</span></a>');
   })
 });
 document.write('<link rel="stylesheet" href="http://static.tumblr.com/4ra4qv5/Kecngi4ls/silkscreen.css"/>');
-document.write('<link rel="stylesheet" href="http://static.tumblr.com/4ra4qv5/fsPngi76f/addons.css"/>');
